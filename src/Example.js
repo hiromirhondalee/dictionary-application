9a3ac6e568acfcaa441eabbf1ec63ba727107700
hiromirhondalee <106946484+hiromirhondalee@users.noreply.github.com>
2022-11-09 10:20:12 +0900

    Created example and synonym component

---
+++ src/Example.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Example(props) {
+  if (props.example) {
+    return (
+      <div className="Example">
+        <strong>Example: </strong>
+        <p>{props.example}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,19 @@
 import React from "react";
+import Example from "./Example";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
+            <strong>Definition: </strong>
+            <p>{definition.definition}</p>
+            <Example example={definition.example} />
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
