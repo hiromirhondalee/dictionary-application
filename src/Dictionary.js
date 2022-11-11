040fb31bf650e6b99b68276898a43db8d71ea14d
hiromirhondalee <106946484+hiromirhondalee@users.noreply.github.com>
2022-11-11 16:28:49 +0900

    Added mages api call and display of images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
-import"./Dictionary.css"
+import Photos from "./Photos";
+import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
+  }
   function search() {
     //documentation for api https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001ca5e15f1cf4e4eae8f195889ed778534";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let header = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: header }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -36,6 +48,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section className="search-section">
+          <h1>What word do you want to lookup?</h1>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
@@ -45,6 +58,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
